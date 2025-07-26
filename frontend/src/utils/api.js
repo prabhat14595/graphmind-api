6906d112fa1d32fd57bde9ec7d0259876a4f1821
prabhatranjan <prabhat.ranjan.pr@gmail.com>
2025-07-26 18:21:26 +0530

    Changes in local host changes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://graphmind-api.onrender.com/chat',  //Change here for localhost
+  baseURL: 'https://graphmind-api.onrender.com/chat',  // Change here for http://localhost:8000/chat
 });
 
 export default api;
