9b49892ce25a1e50554b55e0a6c6c86fc7c54703
prabhatranjan <prabhat.ranjan.pr@gmail.com>
2025-07-26 14:41:41 +0530

    Changes in the api for UI

--- app/nodes.py
+++ app/nodes.py
@@ -6,11 +6,21 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Loads .env into environme
 
+# llm = ChatOpenAI(
+#     # deepseek/deepseek-chat-v3-0324:free
+#     model_name="deepseek/deepseek-chat-v3-0324:free",  # Using free DeepSeek V3 model
+#     base_url="https://openrouter.ai/api/v1",
+#     openai_api_key=os.getenv("OPENROUTER_API_KEY"), #create a .env file with this key
+#     max_tokens=2000  # Reduced for regular chat responses
+#     ) 
+
+
 llm = ChatOpenAI(
-    # deepseek/deepseek-chat-v3-0324:free
-    model_name="deepseek/deepseek-chat-v3-0324:free",  # Using free DeepSeek V3 model
-    base_url="https://openrouter.ai/api/v1",
-    openai_api_key=os.getenv("OPENROUTER_API_KEY"), #create a .env file with this key
+    # Using Groq's Llama3-70B model
+
+    model_name="llama3-70b-8192",
+    base_url="https://api.groq.com/openai/v1",
+    openai_api_key=os.getenv("GROQ_API_KEY"), # Need GROQ_API_KEY in .env file
     max_tokens=2000  # Reduced for regular chat responses
     ) 
 
